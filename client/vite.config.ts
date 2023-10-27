1c385f82ca7875921bfbccdaca32a5b5b6fb6c2d
ASER1989 <aser1989@sina.com>
2023-10-27 03:33:55 +0000

    project init

---
+++ client/vite.config.ts
@@ -0,0 +1,33 @@
+/** @type {import('vite').UserConfig} */
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig(({command, mode}) => {
+    console.log(command, mode)
+    return {
+        root: './',
+        base: './',
+        resolve: {
+            alias: {
+                '@client': path.resolve(__dirname, './src'),
+            },
+        },
+        mode,
+        server: {
+            port: 3127,
+            strictPort: true,
+            hmr: true
+        },
+        plugins: [react()],
+        build: {
+            rollupOptions: {
+                input: ["./index.html"]
+            },
+            outDir: 'dist',
+            sourcemap: command === 'serve',
+            minify: command === 'build'
+        },
+    }
+});
