64ce6b1ddc61c0e357703e3247a5bbf63224703a
ASER1989 <aser1989@sina.com>
2023-10-27 08:43:50 +0000

    test

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,48 @@
-import React from 'react';
-import { Excalidraw } from "@excalidraw/excalidraw";
+import React, { useEffect, useState } from 'react';
+import { Excalidraw, Footer } from "@excalidraw/excalidraw";
+import './app.styl';
+import {
+    BinaryFileData,
+    ExcalidrawImperativeAPI,
+    PointerDownState as ExcalidrawPointerDownState
+} from "@excalidraw/excalidraw/types/types";
+import test from './mocks/test.json';
+import Sidebar from "@client/components/sideBar";
+
 
 export default function App() {
-    return <Excalidraw />;
+    const [docked, setDocked] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [
+        excalidrawAPI,
+        setExcalidrawAPI
+    ] = useState<ExcalidrawImperativeAPI | null>(null);
+
+    useEffect(() => {
+        console.log(excalidrawAPI);
+    }, [excalidrawAPI]);
+
+    const handleContentChange = (event: any) => {
+        console.log(test);
+    }
+
+    const handleShowSidebar = () => {
+        excalidrawAPI?.updateScene(test)
+    }
+
+    return (
+        <Excalidraw initialData={{
+            appState: {
+                openSidebar: {
+                    name: 'history'
+                }
+            }
+        }}
+            ref={(api: ExcalidrawImperativeAPI) => setExcalidrawAPI(api)}
+        >
+            <Sidebar/>
+
+            <Footer />
+        </Excalidraw>
+    );
 }
---
+++ client/src/components/sideBar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.styl';
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar-box'>
+
+        </div>
+    )
+
+}
+export default Sidebar
